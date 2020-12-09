5fdbc9a801cf0129e1a8579961432abbc2a570ec
Aadiyta Vyas <74606669+AV7coder@users.noreply.github.com>
2020-12-09 13:37:37 +0530

    Add files via upload

---
+++ Spammer.py
@@ -0,0 +1,10 @@
+import pyautogui as p # main
+import time # for waiting
+listofwors = ['Follow AV7coder',A cool coder AV7coder is here', 'A young developer AV7coder', Follow AV7coder' ]
+for i in range(0, 5):
+    meassage = listofwors[i]
+    time.sleep(10)
+    p.press('Space') # press space
+    p.write(meassage) # type
+    p.press('Enter') # to send message
+
